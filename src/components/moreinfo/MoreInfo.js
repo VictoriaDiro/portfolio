03292956ebd708b136703e5e020183319f86ffcf
VictoriaDiro <victoriadiro01@gmail.com>
2019-05-27 00:13:29 +0200

    add more information content

--- src/components/moreinfo/MoreInfo.js
+++ src/components/moreinfo/MoreInfo.js
@@ -5,7 +5,80 @@ class MoreInfo extends React.Component {
     return (
       <div className="moreinfo__container">
 
-          <p>Más info</p>
+          <h2 className="work__experience-title">Experiencia Laboral</h2>
+          <ul className="work__experience">
+            <li className="work__experience-item">2018_Auxiliar Administrativo y Atención al Cliente: 24studioLAB (Madrid) – 4 meses</li>
+            <li className="work__experience-item">2013· 2018_Administrativo Contable: Triburbana Hip Hop Store & Montana Shop Madrid (Madrid) – 5 años y 10 meses
+            </li>
+            <li className="work__experience-item">2010· 2018_Equipo de Cámara y Edición en el Sector Audiovisual: Freelance (España) – 7 años y 2 meses
+            </li>
+            <li className="work__experience-item">2016_Diseño de páginas web (Wordpress): Freelance (Madrid) – 4 meses</li>
+            <li className="work__experience-item">2007· 2012_Gerente de tienda online (Prestashop): www.triburbanashop.com (Madrid) – 6 años y 9 meses</li>
+            <li className="work__experience-item">2006· 2010_Dependiente: Triburbana Hip Hop Store (Madrid) – 4 años y 3 meses</li>
+          </ul>
+
+          <h2 className="education">Educación y Formación</h2>
+          <ul className="education__container">
+            <li className="education__item">ACTUALMENTE_Curso de desarrollo Front-End (50h.): Adalab, Google Campus Madrid</li>
+            <li className="education__item">2019_Curso de desarrollo Front-End “DesArrolladoras” (50h.): Universidad Politécnica de Madrid</li>
+            <li className="education__item">2018_Cursos de Fundamentos de HTML, CSS y JavaScript (14h.): Solo Learn</li>
+            <li className="education__item">2018_Certificado profesional en Actividades Administrativas y Relación con el Cliente (680h.): Móstoles Desarrollo, certificación por la Comunidad de Madrid</li>
+            <li className="education__item">2015_ Community Mánager en comercio electrónico (100h.): Rebeldes Marketing Online (Madrid)</li>
+            <li className="education__item">2014_Illustrator CC (14h.): La Casa Encendida (Madrid)</li>
+            <li className="education__item">2011· 2013_Dirección de Fotografía y Operador de Cámara (1440h.): Escuela Superior de Espectáculos TAI (Madrid)</li>
+            <li className="education__item">2009· 2011_Técnico en Laboratorio de Imagen (1400h.): I.E.S. Luis Buñuel (Madrid)</li>
+          </ul>
+
+          <h2 className="skills">Habilidades y conocimientos</h2>
+          <ul className="skills__container">
+            <li className="skills__item">EDICIÓN DE VÍDEO Y FOTOGRAFÍA
+              <ul className="video__skills-container">
+                <li className="video__skills-item">Lightroom, Photoshop, Premiere, Final Cut Pro</li>
+                <li className="video__skills-item">Illustrator, InDesingn, After Effects</li>
+              </ul>
+            </li>
+            <li className="skills__item">SOCIAL MEDIA
+              <ul className="socialmedia__skills-container">
+                <li className="socialmedia__skills-item">Google Analytics, Google Adwords, Facebook Ads, Twitter Ads, SEO, SEM</li>
+              </ul>
+            </li>
+            <li className="skills__item">COMERCIO ELECTRÓNICO
+              <ul className="ecommerce__skills-container">
+                <li className="ecommerce__skills-item">OsCommerce, Prestashop, mailing</li>
+              </ul>
+            </li>
+            <li className="skills__item">PÁGINAS WEB
+              <ul className="web__skills-container">
+                <li className="web__skills-item">Wordpress, Blogger</li>
+              </ul>
+            </li>
+            <li className="skills__item">PROGRAMACIÓN
+              <ul className="develoment__skills-container">
+                <li className="develoment__skills-item">HTML, CSS, Flexbox, Grid, SASS, JavaScript, React</li>
+              </ul>
+            </li>
+            <li className="skills__item">MICROSOFT OFFICE
+              <ul className="office__skills-container">
+                <li className="office__skills-item">PowerPoint, Word, Excel, Access, Power Point, Outlook, Publisher</li>
+              </ul>
+            </li>
+            <li className="skills__item">INTERNET Y REDES SOCIALES
+              <ul className="internetrrss__skills-ontainer">
+                <li className="internetrrss__skills-item">Navegadores, e-mail, Facebook, Instagram, Twitter
+                </li>
+              </ul>
+            </li>
+            <li className="skills__item">ENTORNO
+              <ul className="environment__skills-container">
+                <li className="environment__skills-item">Mac, PC</li>
+              </ul>
+            </li>
+            <li className="skills__item">CONTABILIDAD
+              <ul className="accounting__skills-container">
+                <li className="accounting__skills-item">Factusol</li>
+              </ul>
+            </li>
+          </ul>
 
       </div>
     )
