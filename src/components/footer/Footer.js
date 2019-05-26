14a6a60bd4411c54f46cd224de732cdba8f87b1f
VictoriaDiro <victoriadiro01@gmail.com>
2019-05-27 00:15:56 +0200

    review footer sentence

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render () {
     return (
       <div className="footer__container">
-        <h4 className="make__with">Make with <i className="fas fa-heart"></i> by Victoria Díaz [ HTML, CSS, JavaScript, React ]·················· Background designed by <a href="http://www.freepik.com">Starline / Freepik</a></h4>
+        <h4 className="make__with">Make with <i className="fas fa-heart"></i> by Victoria Diro [ HTML, CSS, JavaScript, React ] ·················· Background designed by <a href="http://www.freepik.com">Starline / Freepik</a></h4>
       </div>
     )
   }
